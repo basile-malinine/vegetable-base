a1500dc1f037c8ec61c61d4331c0b7039a3338f1
Basile Malinine <basile-m@yandex.ru>
2025-02-14 18:19:15 +0300

    FIX: Настройка конфигурации config/web (секция component => i18n).
    * После клонирования из репозитория GitHub в другую папку приложение не запустилось, эта настройка помогла

--- config/web.php
+++ config/web.php
@@ -16,6 +16,18 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages', // if advanced application, set @frontend/messages
+                    'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        //'main' => 'main.php',
+                    ],
+                ],
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'x7RQ0DqHYKr4c2qlN1bEZSP_GEwF-wHx',
